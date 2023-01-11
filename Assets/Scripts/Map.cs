2fe5821264255129a9570b713d416d1a3dd1e363
AliceEverglade <91664294+AliceEverglade@users.noreply.github.com>
2023-01-11 10:34:40 +0100

    Game Scenes created, still working on the Fight Scene

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Map : MonoBehaviour
+{
+    bool ready = false;
+    [SerializeField] private List<GameObject> backgroundImages;
+    [SerializeField] private GameObject currentBackgroundImage;
+    [SerializeField] private GameStateSO gameState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (gameState.EncounterIndex)
+        {
+            case 1:
+                currentBackgroundImage = backgroundImages[0];
+                break;
+            case 3:
+                currentBackgroundImage = backgroundImages[1];
+                break;
+            case 4:
+                currentBackgroundImage = backgroundImages[2];
+                break;
+            case 6:
+                currentBackgroundImage = backgroundImages[3];
+                break;
+            case 9:
+                currentBackgroundImage = backgroundImages[4];
+                break;
+            case 10:
+                currentBackgroundImage = backgroundImages[5];
+                break;
+
+        }
+        Instantiate(currentBackgroundImage);
+        StartCoroutine(Delay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Escape) && ready)
+        {
+            SceneManager.LoadScene("Fight");
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(1f);
+        ready = true;
+    }
+}
---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    bool ready = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Delay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Escape) && ready)
+        {
+            SceneManager.LoadScene("Map");
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(1f);
+        ready = true;
+    }
+}
